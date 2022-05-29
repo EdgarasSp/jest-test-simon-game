10f09d9a005e55f50ee4a44d71037f5fd3c313ef
EdgarasSp <dev.applications.esp@gmail.com>
2022-05-29 17:40:13 +0000

    sino part 3

--- scripts/game.js
+++ scripts/game.js
@@ -8,12 +8,33 @@
 // module.exports = { game }; //use curly braces, when exporting more than one object and function from this file. 
 
 
+// let game = {          SECOND LESSON
+//     currentGame: [],
+//     playerMoves: [],
+//     score: 0,
+//     choices: ["button1", "button2", "button3", "button4"] // later will be linked to newGame()
+// };
+
+
+// module.exports = { game };
+
+
 let game = {
     currentGame: [],
     playerMoves: [],
     score: 0,
-    choices: ["button1", "button2", "button3", "button4"] // later will be linked to newGame()
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+}
 
-module.exports = { game };
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore }; // always remember to export 
